c748f3c0613dc073b9ea46b379422b060f693d31
Alexandru Csete <oz9aec@gmail.com>
2017-08-05 00:22:58 +0200

    Add license

--- dvbs2_tx.cpp
+++ dvbs2_tx.cpp
@@ -1,5 +1,33 @@
-#include <gnuradio/blocks/file_source.h>
+/*
+ * Simple DVB-S2 transmitter application based on gnuradio/gr-dtv
+ *
+ * Copyright 2017 Alexandru Csete OZ9AEC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
+#include <gnuradio/blocks/file_source.h>
 #include <gnuradio/dtv/dvb_bbheader_bb.h>
 #include <gnuradio/dtv/dvb_bbscrambler_bb.h>
 #include <gnuradio/dtv/dvb_bch_bb.h>
@@ -15,7 +43,6 @@
 #include <osmosdr/sink.h>
 #include <signal.h>
 #include <stdio.h>
-
 #include <vector>
 
 
